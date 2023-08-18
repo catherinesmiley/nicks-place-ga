46f307db85fc04c64e3eb26d409c048932656b39
catherinesmiley <catherinesmiley@gmail.com>
2023-08-18 13:47:31 -0400

    add associations to meal and menu models

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,4 @@
 class Meal < ApplicationRecord
+   has_many :menu_meals
+   has_many :menus, through: :menu_meals
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,4 @@
 class Menu < ApplicationRecord
+    has_many :menu_meals
+    has_many :meals, through: :menu_meals
 end
