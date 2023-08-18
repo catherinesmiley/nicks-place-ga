03dbe87a610d2f58938f40717ea75779795ea881
catherinesmiley <catherinesmiley@gmail.com>
2023-08-18 14:08:39 -0400

    create seed data and meals controller actions

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,38 @@
 class MealsController < ApplicationController
+      # GET /meals
+  def index
+    @meals = Meal.all
+
+    render json: @meals
+  end
+
+  # GET /meals/1
+  def show
+    render json: @meal
+  end
+
+  # POST /meals
+  def create
+    @meal = Meal.new(meal_params)
+
+    if @Meal.save
+      render json: @meal, status: :created, location: @meal
+    else
+      render json: @Meal.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /meals/1
+  def update
+    if @Meal.update(meal_params)
+      render json: @meal
+    else
+      render json: @Meal.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /meals/1
+  def destroy
+    @Meal.destroy
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+breakfast_menu = Menu.create(title: "Breakfast", description: "8:00-10:45 AM Fridays & 9:00-10:45 AM Saturdays")
+lunch_dinner_menu = Menu.create(title: "Lunch/Dinner", description: "Hearty meals to fill you up any time of the day!")
+
+biscuits_and_gravy = Meal.create(name: "Biscuits & Gravy", description: "Served with Country Potatoes or Grits. Choice of Sausage or Bacon.", price: 7.50)
+egg_and_cheese_biscuit_sandwich = Meal.create(name: "Egg & Cheese Biscuit Sandwich", description: "Served with Bacon or Sausage", price: 3.50)
+
+chef_salad = Meal.create(name: "Chef Salad", description: "Mixed Greens, Ham, Tomato, Hardboiled Egg, Cucumber & Cheese", price: 9)
+fried_chicken_finger_basket = Meal.create(name: "Fried Chicken Finger Basket", description: "Served with Mac & Cheese, Green Beans, of Fries", price: 7.50)
+grilled_chicken_sandwich = Meal.create(name: "Grilled Chicken Sandwich", description: "Served on a Bun with Mayo, Lettuce & Tomato", price: 8.50)
+
+MenuMeal.create(menu: breakfast_menu, meal: biscuits_and_gravy)
+MenuMeal.create(menu: breakfast_menu, meal: egg_and_cheese_biscuit_sandwich)
+MenuMeal.create(menu: lunch_dinner_menu, meal: chef_salad)
+MenuMeal.create(menu: lunch_dinner_menu, meal: fried_chicken_finger_basket)
+MenuMeal.create(menu: lunch_dinner_menu, meal: grilled_chicken_sandwich)
