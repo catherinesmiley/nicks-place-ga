c39f1926663f98a3c329e6600c6cae43c52ac966
catherinesmiley <catherinesmiley@gmail.com>
2023-08-26 01:03:36 -0400

    add formatted price method to meals model and use in menus show page

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,4 +2,8 @@ class Meal < ApplicationRecord
    has_many :menu_meals
    has_many :menus, through: :menu_meals
    belongs_to :sub_menu, optional: true
+
+   def formatted_price
+      '%.2f' % price
+   end 
 end
