61a3834d2ccba0bb0c4e5a5351b9bb962bbc3da0
catherinesmiley <catherinesmiley@gmail.com>
2023-09-02 15:37:30 -0400

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,44 +6,61 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-breakfast_menu = Menu.find_or_create_by(title: "Breakfast", description: "8:00-10:45 AM Fridays & 9:00-10:45 AM Saturdays")
-lunch_dinner_menu = Menu.find_or_create_by(title: "Lunch/Dinner", description: "Hearty meals to fill you up any time of the day!")
-
-starters = SubMenu.find_or_create_by(name: "Starters", menu: lunch_dinner_menu)
-salads = SubMenu.find_or_create_by(name: "Salads", menu: lunch_dinner_menu)
-baskets_and_plates = SubMenu.find_or_create_by(name: "Baskets & Plates", menu: lunch_dinner_menu)
-kids_menu = SubMenu.find_or_create_by(name: "Kids Menu", menu: lunch_dinner_menu)
-sandwiches_and_burgers = SubMenu.find_or_create_by(name: "Sandwiches & Burgers", menu: lunch_dinner_menu)
-pastas = SubMenu.find_or_create_by(name: "Pastas", menu: lunch_dinner_menu)
-beverages = SubMenu.find_or_create_by(name: "Beverages", menu: lunch_dinner_menu)
-week_day_specials = SubMenu.find_or_create_by(name: "Week Day Specials", menu: lunch_dinner_menu)
-friday_saturday_night_specials = SubMenu.find_or_create_by(name: "Friday & Saturday Night Specials", menu: lunch_dinner_menu)
-
-chef_salad = Meal.find_or_create_by(name: "Chef Salad", description: "Mixed Greens, Ham, Tomato, Hardboiled Egg, Cucumber & Cheese", price: 9)
-fried_chicken_finger_basket = Meal.find_or_create_by(name: "Fried Chicken Finger Basket", description: "Served with Mac & Cheese, Green Beans, of Fries", price: 7.50)
-grilled_chicken_sandwich = Meal.find_or_create_by(name: "Grilled Chicken Sandwich", description: "Served on a Bun with Mayo, Lettuce & Tomato", price: 8.50)
-
-biscuits_and_gravy = Meal.find_or_create_by(name: "Biscuits & Gravy", description: "Served with Country Potatoes or Grits. Choice of Sausage or Bacon.", price: 7.50)
-egg_and_cheese_biscuit_sandwich = Meal.find_or_create_by(name: "Egg & Cheese Biscuit Sandwich", description: "Served with Bacon or Sausage", price: 3.50)
-
-MenuMeal.find_or_create_by(menu: breakfast_menu, meal: biscuits_and_gravy)
-MenuMeal.find_or_create_by(menu: breakfast_menu, meal: egg_and_cheese_biscuit_sandwich)
-MenuMeal.find_or_create_by(menu: lunch_dinner_menu, meal: chef_salad)
-MenuMeal.find_or_create_by(menu: lunch_dinner_menu, meal: fried_chicken_finger_basket)
-MenuMeal.find_or_create_by(menu: lunch_dinner_menu, meal: grilled_chicken_sandwich)
-
-salads.meals << chef_salad 
-baskets_and_plates.meals << fried_chicken_finger_basket
-sandwiches_and_burgers.meals << grilled_chicken_sandwich
-
-specials = [
-    { name: 'Cheeseburger', price: 8.00, day_of_week: 'Monday' },
-    { name: 'Pork Chop', price: 8.50, choice: 'fried or grilled', day_of_week: 'Tuesday' },
-    { name: 'Pork Ribs', price: 8.50, day_of_week: 'Wednesday' },
-    { name: 'Meatloaf', price: 8.00, day_of_week: 'Thursday' },
-    { name: 'Hamburger Steak', price: 8.00, day_of_week: 'Friday' }
-]
-
-specials.each do |special_data|
-    Special.find_or_create_by(special_data)
+def create_menus
+    @breakfast_menu = Menu.find_or_create_by(title: "Breakfast", description: "8:00-10:45 AM Fridays & 9:00-10:45 AM Saturdays")
+    @lunch_dinner_menu = Menu.find_or_create_by(title: "Lunch/Dinner", description: "Hearty meals to fill you up any time of the day!")
   end
+  
+  def create_sub_menus
+    sub_menu_names = [
+        "Starters", "Salads", "Baskets & Plates", "Kids Menu", 
+        "Sandwiches & Burgers", "Pastas", "Beverages", "Weekday Specials",
+        "Friday & Saturday Night Specials"
+    ]
+  
+    @sub_menus = {}
+  
+    sub_menu_names.each do |name|
+        @sub_menus[name.downcase.gsub(' ', '_').to_sym] = SubMenu.find_or_create_by(name: name, menu: @lunch_dinner_menu)
+    end 
+  end
+  
+  def create_meals
+    meals_data = [
+      {name: "Chef Salad", description: "Mixed Greens, Ham, Tomato, Hardboiled Egg, Cucumber & Cheese", price: 9, sub_menu: :salads},
+      {name: "Fried Chicken Finger Basket", description: "Served with Mac & Cheese, Green Beans, of Fries", price: 7.50, sub_menu: :baskets_and_plates},
+      {name: "Grilled Chicken Sandwich", description: "Served on a Bun with Mayo, Lettuce & Tomato", price: 8.50}
+    ]
+  
+    meals_data.each do |meal_data|
+      meal = Meal.find_or_create_by(name: meal_data[:name], description: meal_data[:description], price: meal_data[:price])
+      @sub_menus[meal_data[:sub_menu]].meals << meal
+    end
+  end
+  
+  def create_side_options
+    weekly_sides = ['Fries', 'Coleslaw', 'Mashed Potatoes']
+    @side_option_records = weekly_sides.map { |side_name| SideOption.find_or_create_by(name: side_name) }
+  end
+  
+  def create_specials
+    specials = [
+        { name: 'Cheeseburger', price: 8.00, day_of_week: 'Monday' },
+        { name: 'Pork Chop', price: 8.50, choice: 'fried or grilled', day_of_week: 'Tuesday' },
+        { name: 'Hamburger Steak', price: 8.00, choice: day_of_week: 'Wednesday' },
+        { name: 'Meatloaf', price: 8.00, day_of_week: 'Thursday' },
+        { name: 'Ribs', price: 8.50, day_of_week: 'Friday' }
+    ]
+    
+    specials.each do |special_data|
+        special = Special.find_or_create_by(special_data)
+        special.side_options << @side_option_records
+    end
+  end
+  
+  create_menus
+  create_sub_menus
+  create_meals
+  create_side_options
+  create_specials
+  
